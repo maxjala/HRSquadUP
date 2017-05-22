f597dccc8d7ec3f5067906fafb774b3d0c35a539
nicholaslee <nicholaslee@nicholaslees-MacBook-Pro.local>
2017-05-22 19:43:12 +0800

    worked on images and ui

--- SquadUpHR/SkillCategory.swift
+++ SquadUpHR/SkillCategory.swift
@@ -27,12 +27,12 @@ class SkillCategory {
     
     static func fetchAllCategories() -> [SkillCategory] {
         return [
-            SkillCategory(title: "Management", featuredImage: UIImage(named: "desgin")!, color: UIColor(red: 240/255.0, green: 133/255.0, blue: 91/255.0, alpha: 0.8)),
-            SkillCategory(title: "Accounting", featuredImage: UIImage(named: "desgin")!, color: UIColor(red: 105/255.0, green: 80/255.0, blue: 227/255.0, alpha: 0.8)),
+            SkillCategory(title: "Management", featuredImage: UIImage(named: "Management")!, color: UIColor(red: 240/255.0, green: 133/255.0, blue: 91/255.0, alpha: 0.8)),
+            SkillCategory(title: "Accounting", featuredImage: UIImage(named: "accounting")!, color: UIColor(red: 105/255.0, green: 80/255.0, blue: 227/255.0, alpha: 0.8)),
             SkillCategory(title: "Design", featuredImage: UIImage(named: "desgin")!, color: UIColor(red: 245/255.0, green: 62/255.0, blue: 40/255.0, alpha: 0.8)),
-            SkillCategory(title: "IT", featuredImage: UIImage(named: "desgin")!, color: UIColor(red: 103/255.0, green: 217/255.0, blue: 87/255.0, alpha: 0.8)),
+            SkillCategory(title: "IT", featuredImage: UIImage(named: "Info")!, color: UIColor(red: 103/255.0, green: 217/255.0, blue: 87/255.0, alpha: 0.8)),
             
-            SkillCategory(title: "Education", featuredImage: UIImage(named: "desgin")!, color: UIColor(red: 150/255.0, green: 102/255.0, blue: 102/255.0, alpha: 0.8)),
+            SkillCategory(title: "Education", featuredImage: UIImage(named: "edu1")!, color: UIColor(red: 150/255.0, green: 102/255.0, blue: 102/255.0, alpha: 0.8)),
             SkillCategory(title: "Others", featuredImage: UIImage(named: "desgin")!, color: UIColor(red: 63/255.0, green: 51/255.0, blue: 80/255.0, alpha: 0.8))
             
             
